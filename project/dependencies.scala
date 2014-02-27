8c636937a26be93b7466c74c44f5a7cf74f37bfb
Michael Wellner <info@michaelwellner.de>
2014-02-28 00:37:39 +0100

    sc

--- project/dependencies.scala
+++ project/dependencies.scala
@@ -15,27 +15,23 @@ object dependencies {
   private def container(deps: ModuleID*): Seq[ModuleID] = deps map (_ % "container")
   
   // format: OFF
-  private lazy val bootstrap		= "org.webjars" 							% 	"bootstrap" 			% "3.0.1"
-  private lazy val c10n				= "c10n" 									% 	"c10n-core" 			% "1.1"
-  private lazy val commonslang3 	= "org.apache.commons" 						% 	"commons-lang3" 		% "3.1"
-  private lazy val config 			= "com.typesafe" 							% 	"config" 				% "1.0.2"
-  private lazy val eclipselink 		= "org.eclipse.persistence"					% 	"eclipselink" 			% "2.5.1"
-  private lazy val gumby			= "org.webjars" 							% 	"gumby" 				% "2.4.6"
-  private lazy val h2jdbc 			= "com.h2database" 							% 	"h2" 					% "1.3.170"
-  private lazy val itext			= "com.lowagie" 							% 	"itext" 				% "4.2.1"
-  private lazy val knockout			= "org.webjars" 							% 	"knockout" 				% "3.0.0"
-  private lazy val markdown4j 		= "org.commonjava.googlecode.markdown4j" 	% 	"markdown4j" 			% "2.2-cj-1.0"
-  private lazy val pdfbox			= "org.apache.pdfbox" 						% 	"pdfbox" 				% "1.8.3"
-  private lazy val playSlick		= "com.typesafe.play" 						%% 	"play-slick" 			% "0.5.0.8" 
-  private lazy val playJDBC			= "com.typesafe.play" 						%%  "play-jdbc"  			% "2.2.1"
-  private lazy val scalareflect 	= "org.scala-lang" 							% 	"scala-reflect" 		% "2.10.3"
-  private lazy val slf4jnop 		= "org.slf4j" 								% 	"slf4j-nop" 			% "1.6.4"
-  private lazy val slick 			= "com.typesafe.slick" 						%% 	"slick" 				% "1.0.1"
-  private lazy val stackcontroller	= "jp.t2v" 									%% "stackable-controller" 	% "0.3.0"
-  private lazy val twitter4j		= "org.twitter4j"							%	"twitter4j-core"		% "3.0.5"
-  private lazy val twittertext		= "com.twitter"								% 	"twitter-text"			% "1.6.1"
-  private lazy val utillogging 		= "com.twitter" 							%% 	"util-logging" 			% "6.5.0"
-  private lazy val webjarsplay		= "org.webjars" 							%% 	"webjars-play" 			% "2.2.0"
+  private lazy val bootstrap	  	  = "org.webjars" 				            			% 	"bootstrap" 		    	  % "3.0.1"
+  private lazy val c10n				      = "c10n" 								                	% 	"c10n-core" 	    		  % "1.1"
+  private lazy val commonslang3    	= "org.apache.commons" 			        			% 	"commons-lang3" 	  	  % "3.1"
+  private lazy val config 			    = "com.typesafe" 					            		% 	"config" 				        % "1.0.2"
+  private lazy val eclipselink 		  = "org.eclipse.persistence"	      				% 	"eclipselink" 			    % "2.5.1"
+  private lazy val h2jdbc 			    = "com.h2database" 						          	% 	"h2" 				           	% "1.3.170"
+  private lazy val knockout			    = "org.webjars" 							            % 	"knockout" 				      % "3.0.0"
+  private lazy val markdown4j 		  = "org.commonjava.googlecode.markdown4j" 	% 	"markdown4j" 			      % "2.2-cj-1.0"
+  private lazy val playSlick		    = "com.typesafe.play" 			        			%% 	"play-slick" 			      % "0.5.0.8"
+  private lazy val scalareflect 	  = "org.scala-lang" 					            	% 	"scala-reflect" 		    % "2.10.3"
+  private lazy val slf4jnop 		    = "org.slf4j" 							          	  % 	"slf4j-nop" 			      % "1.6.4"
+  private lazy val slick 			      = "com.typesafe.slick" 			        			%% 	"slick" 				        % "1.0.1"
+  private lazy val stackcontroller	= "jp.t2v" 							              		%% "stackable-controller" 	% "0.3.0"
+  private lazy val twitter4j		    = "org.twitter4j"				          			  %	"twitter4j-core"		      % "3.0.5"
+  private lazy val twittertext		  = "com.twitter"				            				% 	"twitter-text"			    % "1.6.1"
+  private lazy val utillogging 		  = "com.twitter" 			            				%% 	"util-logging" 			    % "6.5.0"
+  private lazy val webjarsplay		  = "org.webjars" 			            				%% 	"webjars-play" 			    % "2.2.0"
   // format: ON
 
   /**
@@ -70,9 +66,6 @@ object dependencies {
   	
   lazy val wellnrBlogDependencies = compile(
   	markdown4j,
-  	playSlick, 
-  	slick,
-  	stackcontroller,
   	twitter4j,
   	twittertext)
 }
